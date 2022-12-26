4ad3f4b8e71b5ff32501c66317ce67edeb7598ca
Serhii <sergenadoenko@gmail.com>
2022-12-26 18:30:17 +0100

    1:structured into separate components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,15 @@
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarian</li>
+        <li>Grill</li>
+        <li>Hot</li>
+        <li>Closed </li>
+      </ul>
+    </div>
+  );
+};
+export default Categories;

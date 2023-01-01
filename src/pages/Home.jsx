f76d7e6de875b5080195f92e3f01752d30ab4a0b
Serhii <sergenadoenko@gmail.com>
2023-01-01 18:11:25 +0100

    12:  used createAsyncThunk and created Slice where the response is processing from the server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
+import { useEffect, useContext } from 'react';
 
 import { SearchContext } from '../App';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { fetchPizzas } from '../redux/slices/pizzaSlice';
 import Sort from '../components/Sort';
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
@@ -13,34 +13,38 @@ import NotFoundItems from '../components/NotFoundItemsBlock';
 
 const Home = () => {
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+  const { items, status } = useSelector((state) => state.pizza);
   const dispatch = useDispatch();
 
   const { searchValue } = useContext(SearchContext);
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   const onChangePage = (number) => {
     dispatch(setCurrentPage(number));
+  };
+  const getPizzas = async () => {
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sort.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+    dispatch(
+      fetchPizzas({
+        order,
+        sortBy,
+        category,
+        search,
+        currentPage,
+      }),
+    );
+    window.scrollTo(0, 0);
   };
 
   const pizzas = items.map((el) => <PizzaBlock key={el.id} {...el} />);
   const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
 
   useEffect(() => {
-    setIsLoading(true);
-    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
-    const sortBy = sort.sortProperty.replace('-', '');
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
-    const search = searchValue ? `&search=${searchValue}` : '';
-    axios
-      .get(
-        `https://63ab08e4fdc006ba6053b2ca.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-      )
-      .then((res) => {
-        setItems(res.data);
-        setIsLoading(false);
-      });
-    window.scrollTo(0, 0);
+    getPizzas();
+    /* eslint-disable */
   }, [categoryId, sort.sortProperty, currentPage, searchValue]);
 
   return (
@@ -50,13 +54,22 @@ const Home = () => {
 
         <Sort />
       </div>
-      <h2 className="content__title"></h2>
-      {/* <div className="content__items">{isLoading ? skeletons : pizzas}</div> */}
-      {searchValue !== '' && pizzas.length === 0 ? (
+      <h2 className="content__title">All pizzas</h2>
+      {status === 'error' ? (
+        <div className="content__error-info">
+          <h2>something's wrong🤔</h2>
+          <p>failed to receive the goods. Please, try again later</p>
+        </div>
+      ) : searchValue !== '' && pizzas.length === 0 ? (
         <NotFoundItems />
       ) : (
-        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+        <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
       )}
+      {/*  {searchValue !== '' && pizzas.length === 0 ? (
+        <NotFoundItems />
+      ) : (
+        <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
+      )} */}
       {categoryId === 0 ? (
         <Pagination currentPage={currentPage} onChangePage={onChangePage} />
       ) : null}
